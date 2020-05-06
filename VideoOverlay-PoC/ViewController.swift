88456fdefa58fc0bfe3d0df3ff03cae6c6ed3c51
lofo <louis.fo@gmail.com>
2020-05-06 16:34:09 +0200

    Initial Commit

---
+++ VideoOverlay-PoC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoOverlay-PoC
+//
+//  Created by Louis Fournier on 5/6/20.
+//  Copyright © 2020 Louis Fournier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
