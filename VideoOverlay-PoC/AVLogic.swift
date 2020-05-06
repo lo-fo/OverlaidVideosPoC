1ae98f22c5dcbb26a0a7bc1eb44364d387d54eb2
lofo <louis.fo@gmail.com>
2020-05-06 19:09:39 +0200

    overlay now overlapping cut

--- VideoOverlay-PoC/AVLogic.swift
+++ VideoOverlay-PoC/AVLogic.swift
@@ -19,6 +19,10 @@ final class Renderer {
                 guard let bgURL = Bundle.main.url(forResource: "IMG_7277", withExtension: "mp4") else { return nil }
                 return AVAsset(url: bgURL)
             }(),
+            let bgAsset2 = { () -> AVAsset? in
+                guard let bgURL = Bundle.main.url(forResource: "IMG_7248", withExtension: "mp4") else { return nil }
+                return AVAsset(url: bgURL)
+            }(),
             let fgAsset = { () -> AVAsset? in
                 guard let bgURL = Bundle.main.url(forResource: "7A49A023-7DC2-4C62-AD48-3D489FBAC598", withExtension: "mov") else { return nil }
                 return AVAsset(url: bgURL)
@@ -27,24 +31,34 @@ final class Renderer {
         let composition = AVMutableComposition()
         guard
             let bgTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid),
+            let bgTrack2 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid),
             let fgTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid),
             let bgVideoTrack = bgAsset.tracks(withMediaType: .video).first,
+            let bgVideoTrack2 = bgAsset2.tracks(withMediaType: .video).first,
             let fgVideoTrack = fgAsset.tracks(withMediaType: .video).first
             else { fatalError() }
+        /* Compute times to make fgAsset overlap on the cutting point between the two bgAssets */
+        let halfLength = CMTimeMultiplyByRatio(fgAsset.duration, multiplier: -1, divisor: 2)
+        let insertionTime = CMTimeAdd(bgAsset.duration, halfLength)
+        /* end of computation */
         do {
         // Note: maybe instead of bgVideoTrack.timeRange -> CMTimeRangeMake(start: .zero, duration: fgAsset.duration)
             try bgTrack.insertTimeRange(bgVideoTrack.timeRange, of: bgVideoTrack, at: .zero)
-            try fgTrack.insertTimeRange(fgVideoTrack.timeRange, of: fgVideoTrack, at: .zero)
+            try bgTrack2.insertTimeRange(bgVideoTrack2.timeRange, of: bgVideoTrack2, at: CMTimeAdd(.zero, bgAsset.duration))
+            try fgTrack.insertTimeRange(fgVideoTrack.timeRange, of: fgVideoTrack, at: insertionTime)
         } catch {
             fatalError()
         }
         /* Setup Video Composition */
         let instruction = AVMutableVideoCompositionInstruction()
-        instruction.timeRange = bgTrack.timeRange // 🤔
+        instruction.timeRange = CMTimeRangeMake(start: .zero, duration: CMTimeAdd(bgAsset.duration, bgAsset2.duration)) // 🤔
         let fgLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: fgTrack)
+        fgLayerInstruction.setOpacity(0.0, at: CMTimeAdd(insertionTime, fgAsset.duration))
         let bgLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: bgTrack)
+        bgLayerInstruction.setOpacity(0.0, at: bgAsset.duration)
+        let bgLayerInstruction2 = AVMutableVideoCompositionLayerInstruction(assetTrack: bgTrack2)
         /* ordered from fg to bg */
-        instruction.layerInstructions = [fgLayerInstruction, bgLayerInstruction]
+        instruction.layerInstructions = [fgLayerInstruction, bgLayerInstruction, bgLayerInstruction2]
         let videoComposition = AVMutableVideoComposition(propertiesOf: composition)
         videoComposition.frameDuration = bgTrack.minFrameDuration // 🤔
         videoComposition.renderSize = bgTrack.naturalSize // 🤔
